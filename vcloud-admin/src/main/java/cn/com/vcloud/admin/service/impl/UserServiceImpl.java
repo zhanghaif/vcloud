05b8929e44c52b33d1f1fb031581c9048b91305e
zhanghaifeng <123892897@qq.com>
2019-04-09 09:20:15 +0800

    修正创建账号重复提示错误问题

--- vcloud-admin/src/main/java/cn/com/vcloud/admin/service/impl/UserServiceImpl.java
+++ vcloud-admin/src/main/java/cn/com/vcloud/admin/service/impl/UserServiceImpl.java
@@ -3,19 +3,15 @@ package cn.com.vcloud.admin.service.impl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.annotation.Resource;
-
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import cn.com.vcloud.admin.mapper.PermissionMapper;
 import cn.com.vcloud.admin.mapper.UserMapper;
 import cn.com.vcloud.admin.mapper.UserRoleMapper;
 import cn.com.vcloud.admin.model.entity.SysUser;
-import cn.com.vcloud.admin.model.entity.SysUserRole;
 import cn.com.vcloud.admin.service.UserService;
 import cn.com.vcloud.core.service.AbstractService;
 
@@ -73,11 +69,11 @@ public class UserServiceImpl extends AbstractService<SysUser> implements UserSer
     }
     
     /**
-     * 重写save方法，密码加密后再存
+     * *重写save方法，密码加密后再存
      */
     @Override
     public void save(final SysUser user) {
-    	SysUser u = this.findBy("account", user.getUsername());
+    	SysUser u = this.findBy("account", user.getAccount());
         if (u != null) {
         	throw new UsernameNotFoundException("account already existed");
         } else {
@@ -87,20 +83,18 @@ public class UserServiceImpl extends AbstractService<SysUser> implements UserSer
                 this.userMapper.insertSelective(user);
                 //log.info("User<{}> id : {}", user.getUsername(), user.getId());
                 // 如果没有指定角色Id，以默认普通用户roleId保存
-                Long roleId = user.getRoleId();
-                if (roleId == null) {
-                    roleId = 2L;
-                }
-                SysUserRole sysUserRole = new SysUserRole();
-                sysUserRole.setUserId(user.getId());
-                sysUserRole.setRoleId(roleId);
-                this.userRoleMapper.insert(sysUserRole);
+			/*
+			 * Long roleId = user.getRoleId(); if (roleId == null) { roleId = 2L; }
+			 * SysUserRole sysUserRole = new SysUserRole();
+			 * sysUserRole.setUserId(user.getId()); sysUserRole.setRoleId(roleId);
+			 * this.userRoleMapper.insert(sysUserRole);
+			 */
             
         }
     }
     
     /**
-     * 重写update方法
+     ** 重写update方法
      */
     @Override
     public void update(final SysUser user) {
